6f7412a2c396eb3c91e69b67a537ff43c4707674
Anton Novikov <an146@ya.ru>
2012-05-20 17:44:23 +0400

    depend on gstreamer0.11; print pipeline messages

--- player.py
+++ player.py
@@ -10,17 +10,13 @@ class Player:
         self.playbin = None
 
         self.bus = self.pipeline.get_bus()
-        self.bus.connect('message::eos', self.on_eos)
-        self.bus.connect('message::tag', self.on_tag)
-        self.bus.connect('message::error', self.on_error)
-        self.bus.connect('message::buffering', self.on_buffering)
-        #self.bus.connect('message', self.on_message)
+        self.bus.connect('message', self.on_message)
         self.bus.add_signal_watch()
 
     def play(self, uri):
         self.stop()
-        print("playing {0}".format(uri), file=sys.stderr)
-        self.playbin = Gst.ElementFactory.make("playbin2", None)
+        print("playing", uri, file=sys.stderr)
+        self.playbin = Gst.ElementFactory.make("playbin", None)
         self.playbin.set_property('uri', uri)
         self.pipeline.add(self.playbin)
         self.pipeline.set_state(Gst.State.PLAYING)
@@ -36,25 +32,14 @@ class Player:
             self.pipeline.remove(self.playbin)
         self.playbin = None
 
-    def on_eos(self, bus, msg):
-        print('on_eos', file=sys.stderr)
-        self.pipeline.set_state(Gst.State.NULL)
-        #self.mainloop.quit()
-
-    def on_tag(self, bus, msg):
-        taglist = msg.parse_tag()
-        print('on_tag:', file=sys.stderr)
-        for key in taglist.keys():
-            print("{0} = {1}".format(key, taglist[key]), file=sys.stderr)
-
-    def on_error(self, bus, msg):
-        error = msg.parse_error()
-        print('on_error:', error[1], file=sys.stderr)
-        #self.mainloop.quit()
-
-    def on_buffering(self, bus, msg):
-        buffering = msg.parse_buffering()
-        print('on_buffering: {0}'.format(buffering), file=sys.stderr)
-
     def on_message(self, bus, msg):
-        print('message: {0}'.format(type(msg)), file=sys.stderr)
+        t = msg.type.first_value_nick
+        if t == 'error':
+            print('error:', msg.parse_error(), file=sys.stderr)
+        elif t == 'state_changed':
+            st = list(map(lambda x: x.value_nick, msg.parse_state_changed()))
+            print('state_changed:', st, file=sys.stderr)
+        elif t == 'element':
+            print('element:', msg.get_structure(), file=sys.stderr)
+        else:
+            print('message:', t, file=sys.stderr)
